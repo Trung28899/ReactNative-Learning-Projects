a5c9c640800054d5e5299d28547b0ff465232729
Trung28899 <trung28899@gmail.com>
2021-02-17 00:33:15 -0500

    init commit

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import StartApp from "./App/StartApp/StartApp";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <StartApp />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    color: "#ccc",
+    fontSize: 16,
+    alignItems: "center",
   },
 });
---
+++ App/StartApp/StartApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import styles from "./StartAppStyle";
+
+const StartApp = () => {
+  return (
+    <View style={styles.appContainer}>
+      <Text style={styles.text}>HELLO CAC BAN, TOI LA TRUNG</Text>
+    </View>
+  );
+};
+
+export default StartApp;
